1de04b77814ff8d8e77bb41eadc1efce1a74727c
Jensdboc <jensdb@telenet.be>
2022-02-17 14:23:03 +0100

    help w/ overview and categories

--- Help.py
+++ Help.py
@@ -9,16 +9,28 @@ class CustomHelpCommand(commands.HelpCommand):
         super().__init__()
 
     async def send_bot_help(self, mapping):
-        await self.get_destination().send('This is not implemented yet but you can use !help <command>')
-        return
+        embed = discord.Embed(title="Help overview", description="Use !help <category> for more information. **First letter has to be capitilized!**")
         for cog in mapping:
             if cog:
-                # get_destination: Calls destination a.k.a. where you want to send the command
-                await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in mapping[cog]]}') 
+                list = ""
+                for command in cog.get_commands():
+                    list += command.name + ', '
+                embed.add_field(name = cog.qualified_name, value = list[:-2])
+        await self.get_destination().send(embed=embed) # get_destination: Calls destination a.k.a. where you want to send the command
                 
     async def send_cog_help(self, cog):
-        #await self.get_destination().send(f'{cog.qualified_name}: {[command.name for command in cog.get_commands()]}')
-        await self.get_destination().send('This is not implemented yet but you can use !help <command>')
+        if cog:
+            embed = discord.Embed(title="Help " + cog.qualified_name, description="Use !help <command> for more information.")
+            list = ""
+            for command in cog.get_commands():
+                list += command.usage
+                for alias in command.aliases:
+                    list += ', [!' + alias + ']'
+                list += ': ' + command.description + "\n"
+            embed.add_field(name = cog.qualified_name, value = list[:-2])
+            await self.get_destination().send(embed=embed)
+        else:
+            await self.get_destination().send("This is not a valid cog.")
 
     async def send_group_help(self, group):
         #await self.get_destination().send(f'{group.name}: {[command.name for index, command in enumerate(group.commands)]}')
@@ -29,7 +41,8 @@ class CustomHelpCommand(commands.HelpCommand):
         for alias in command.aliases:
             title += ', [!' + alias + ']'
         embed = discord.Embed(title=title, description=command.description)
-        embed.add_field(name="Syntax:", value=command.usage, inline=False)
+        if command.usage != '':
+            embed.add_field(name="Syntax:", value=command.usage, inline=False)
         if command.help != '':
-            embed.add_field(name="Example:", value=command.help, inline=False)
+            embed.add_field(name="Extra:", value=command.help, inline=False)
         await self.get_destination().send(embed=embed)
