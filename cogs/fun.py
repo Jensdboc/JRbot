1697edb6199184c89446af3c1a5340526d535faf
Jensdboc <jensdb@telenet.be>
2023-12-04 19:51:18 +0100

    Fix lurs

--- cogs/fun.py
+++ cogs/fun.py
@@ -8,6 +8,8 @@ import nltk
 
 MAX_TITLE_LENGTH = 256
 LARS_CHANNEL_ID = 1178007345243103302
+
+
 class Fun(commands.Cog):
 
     def __init__(self, client):
@@ -168,20 +170,20 @@ class Fun(commands.Cog):
     @commands.command(usage="!lars <correction>",
                       description="The wrong message will be posted into the channel with the correction underneath",
                       help="This message will be posted in the channel of Lars. Reply to the message that contains a mistake.")
-    async def lars(self, ctx: commands.Context, id: str, *, description: str=None):
+    async def lars(self, ctx: commands.Context, *, description: str = None):
         lars_channel = self.client.get_channel(LARS_CHANNEL_ID)
         if ctx.message.reference is not None:
             lars_message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
             if lars_message:
                 if lars_message.author.id != 350243309270335489:
                     embed = discord.Embed(title="Woops...", description="This message was not from Lars!")
-                    await ctx.reply(embed=embed) 
+                    await ctx.reply(embed=embed)
                 else:
                     embed = discord.Embed(title=lars_message.content, description=description)
                     await lars_channel.send(embed=embed)
         else:
             embed = discord.Embed(title="Woops...", description="You did not reply to a message of Lars!")
-            await ctx.reply(embed=embed) 
+            await ctx.reply(embed=embed)
 
     @commands.Cog.listener("on_message")
     async def taylor(self, message):
@@ -209,6 +211,7 @@ class Fun(commands.Cog):
             emoji = "\U0001F1F1"  # regional_indicator_l
             if present:
                 await message.add_reaction(emoji)
+
 
 # Allows to connect cog to bot
 async def setup(client):
