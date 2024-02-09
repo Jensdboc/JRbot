38dc8c05ded7137acbb9a69a4b2cb682aefc9331
Norick Beterams <norick.beterams@ugent.be>
2024-02-10 00:20:59 +0100

    separate draw file

--- cogs/poker.py
+++ cogs/poker.py
@@ -2,13 +2,12 @@ import os
 import pickle
 from typing import List, Union
 
-import requests
-from PIL import Image, ImageDraw, ImageFont, ImageOps
+from PIL import Image, ImageDraw, ImageFont
 
 import discord
 from discord.ext import commands
 
-from poker.constants import player_places
+from poker.draw import create_avatars_for_player
 from poker.game import Game
 
 
@@ -46,39 +45,6 @@ def load_poker_games_from_file(input_file: str) -> Games:
         games = pickle.load(file)
 
     return games
-
-
-def circular_avatar(image, size):
-    mask = Image.new("L", size, 0)
-    draw = ImageDraw.Draw(mask)
-    draw.ellipse((0, 0) + size, fill=255)
-    output = ImageOps.fit(image, mask.size, centering=(0.5, 0.5))
-    output.putalpha(mask)
-    return output
-
-
-def create_avatars_for_player(reaction, player, current_game, player_background):
-    avatar_size = (210, 200)
-
-    for p, player_place in zip(current_game.players[current_game.get_player_index(player.player_id) + 1:] + current_game.players[:current_game.get_player_index(player.player_id)], player_places):
-        discord_user = reaction.message.guild.get_member(p.player_id)
-        if not os.path.exists(f"data_pictures/avatars/{discord_user.id}.png"):
-            avatar = discord_user.display_avatar
-            if avatar is None:
-                avatar = discord_user.default_avatar
-
-            with requests.get(avatar.url) as r:
-                img_data = r.content
-            with open(f"data_pictures/avatars/{discord_user.id}.png", 'wb') as handler:
-                handler.write(img_data)
-
-            player_avatar = circular_avatar(Image.open(f"data_pictures/avatars/{discord_user.id}.png").convert('RGBA').resize(avatar_size), avatar_size)
-            player_avatar.save(f"data_pictures/avatars/{discord_user.id}.png")
-
-        player_avatar = Image.open(f"data_pictures/avatars/{discord_user.id}.png")
-        player_background.paste(player_avatar, player_place, player_avatar)
-
-    return player_background
 
 
 class Poker(commands.Cog):
@@ -186,8 +152,8 @@ class Poker(commands.Cog):
 
                 player_background.save(f"data_pictures/poker/message_{player.player_id}.png")
 
-                user = await self.client.fetch_user(player.player_id)
-                await user.send(file=discord.File(f"data_pictures/poker/message_{player.player_id}.png"))
+                discord_user = await self.client.fetch_user(player.player_id)
+                await discord_user.send(file=discord.File(f"data_pictures/poker/message_{player.player_id}.png"))
                 player_background.close()
             poker_background.close()
 
---
+++ poker/draw.py
@@ -0,0 +1,39 @@
+import os
+
+import requests
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+
+from poker.constants import player_places
+
+
+def circular_avatar(image, size):
+    mask = Image.new("L", size, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + size, fill=255)
+    output = ImageOps.fit(image, mask.size, centering=(0.5, 0.5))
+    output.putalpha(mask)
+    return output
+
+
+def create_avatars_for_player(reaction, player, current_game, player_background):
+    avatar_size = (210, 200)
+
+    for p, player_place in zip(current_game.players[current_game.get_player_index(player.player_id) + 1:] + current_game.players[:current_game.get_player_index(player.player_id)], player_places):
+        discord_user = reaction.message.guild.get_member(p.player_id)
+        if not os.path.exists(f"data_pictures/avatars/{discord_user.id}.png"):
+            avatar = discord_user.display_avatar
+            if avatar is None:
+                avatar = discord_user.default_avatar
+
+            with requests.get(avatar.url) as r:
+                img_data = r.content
+            with open(f"data_pictures/avatars/{discord_user.id}.png", 'wb') as handler:
+                handler.write(img_data)
+
+            player_avatar = circular_avatar(Image.open(f"data_pictures/avatars/{discord_user.id}.png").convert('RGBA').resize(avatar_size), avatar_size)
+            player_avatar.save(f"data_pictures/avatars/{discord_user.id}.png")
+
+        player_avatar = Image.open(f"data_pictures/avatars/{discord_user.id}.png")
+        player_background.paste(player_avatar, player_place, player_avatar)
+
+    return player_background
